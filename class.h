37b19eae503b14bd42fa2b5e0c43e9f980f55b31
notalochka <127450636+notalochka@users.noreply.github.com>
2023-04-06 22:59:14 +0300

    Add files via upload

---
+++ class.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+#ifndef CLASS
+
+using namespace std;
+class Text {
+public:
+    string text;
+public:
+    Text() {}
+    Text(string s) : text(s) {}
+
+    string getText() const;
+    void setText(string s);
+    void appendText(string s);
+    double percentDigits();
+
+};
+#endif // !CLASS
